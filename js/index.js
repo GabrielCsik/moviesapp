0927e0d2b96ed621b044dcd2fac6abbd7c08144b
Gabriel Csík <gabriel.csik07@gmail.com>
2019-11-29 12:47:03 +0100

    http request done

--- js/index.js
+++ js/index.js
@@ -1,46 +1,31 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+document.addEventListener("deviceready", onDeviceReady, false);
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+function onDeviceReady() {
+  const urlBegin = "https://api.themoviedb.org/3/search/movie?api_key=";
+  const API_KEY = "bbd92ee26b128295e6b83f95ab79c675";
+  const inputField = document.getElementById("inputField");
+  const submitButton = document.getElementById("submitButton");
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+  submitButton.addEventListener("click", function() {
+    const value = inputField.value;
+    const url = urlBegin + API_KEY + "&query=" + value;
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    // fetch(url)
+    //   .then((res) => res.json())
+    //   .then(data => {
+    //     console.log(data);
+    //   })
+    //   .catch(error => {
+    //     console.log("error", error);
+    //   });
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            console.log(xhr.response);
+        }  
     }
-};
-
-app.initialize();
+    xhr.open("GET", url);
+    xhr.send();
+  });
+}
